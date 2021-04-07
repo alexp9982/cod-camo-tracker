14f8381155c0b7dad7745aa4612b1346307d3ff8
alexp9982 <alexap9982@gmail.com>
2021-04-07 13:17:33 -0400

    Added welcome message

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,10 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" :style="{ overflowY: mobileNav ? 'hidden' : null }">
+    <transition name="slidedown">
+      <div class="notice" v-if="showNotice">
+        <p>Welcome to the Call of Duty Camo Tracker. Enjoy!</p>
+      <eva-icon name="close" fill="white" width="16" height="16" @click="showNotice = false"></eva-icon>
+      </div>
+    </transition>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
