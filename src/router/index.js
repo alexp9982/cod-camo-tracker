a50dfe357705c30c115c5cfa7a05ddd98d6f5757
alexp9982 <alexap9982@gmail.com>
2021-04-08 00:43:44 -0400

    Added more pages to the router system

--- src/router/index.js
+++ src/router/index.js
@@ -32,7 +32,7 @@ const routes = [
     name: 'About',
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
-  {
+{
     path: '/settings',
     name: 'Settings',
     component: () => import(/* webpackChunkName: "settings" */ '../views/Settings.vue')
---
+++ src/views/mw2019.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "mw2019"
+};
+</script>
+
+<style scoped>
+
+</style>
