a08f095b2e95ca613b654f8f2fb20a2842027caf
alexp9982 <alexap9982@gmail.com>
2021-04-09 10:13:08 -0400

    Linked Settings page

--- src/router/index.js
+++ src/router/index.js
@@ -32,11 +32,11 @@ const routes = [
     name: 'About',
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
-/*{
+{
     path: '/settings',
     name: 'Settings',
-    component: () => import(/!* webpackChunkName: "settings" *!/ '../views/Settings.vue')
-  }*/
+    component: () => import(/* webpackChunkName: "settings" */ '../views/Settings.vue')
+  }
 ]
 
 const router = new VueRouter({
